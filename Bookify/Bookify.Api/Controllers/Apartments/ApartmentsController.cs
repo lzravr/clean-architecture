54b7d0f53ae0287a409c245c41ed6e484064aeb7
Lazar Avramovic <lzr.avr@gmail.com>
2023-08-21 16:10:02 +0200

    half of api

---
+++ Bookify/Bookify.Api/Controllers/Apartments/ApartmentsController.cs
@@ -0,0 +1,26 @@
+﻿using Bookify.Application.Apartments.SearchApartments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers.Apartments;
+[Route("api/apartments")]
+[ApiController]
+public class ApartmentsController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ApartmentsController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SearchApartments(DateOnly startDate, DateOnly endDate, CancellationToken token)
+    {
+        var query = new SearchAparmentsQuery(startDate, endDate);
+
+        var result = await _sender.Send(query, token);
+
+        return Ok(result.Value);
+    }
+}
---
+++ Bookify/Bookify.Api/Program.cs
@@ -0,0 +1,30 @@
+using Bookify.Api.Extensions;
+using Bookify.Application;
+using Bookify.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.AddMigrations();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
